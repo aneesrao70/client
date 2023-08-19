82d47d304a696862dda6ec27117738ed6fce9292
aneesrao70 <aneesrao70@gmail.com>
2023-08-20 00:02:10 +0500

    build

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 
 import axios from 'axios';
+const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 const backendPort = 5000; // Replace this with your actual backend port
 
 const api = axios.create({
-  baseURL: `http://localhost:${backendPort}`,
+  baseURL: `http://localhost:${serverUrl}`,
 });
 
 export default api;
