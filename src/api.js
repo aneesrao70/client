3e42bec995df2798fee1d5de95a0da8a0658a37f
aneesrao70 <aneesrao70@gmail.com>
2023-08-20 09:40:15 +0500

    3

--- src/api.js
+++ src/api.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 const backendPort = 5000; // Replace this with your actual backend port
 
 const api = axios.create({
-  baseURL: `http://localhost:${backendPort}`,
+  baseURL: `https://inventory-manager-k3zf.onrender.com`,
+  /* http://localhost:${backendPort} */
 });
 
 export default api;
