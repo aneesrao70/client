cc4822756405efb3d24c5896bbf74b3b3323e404
aneesrao70 <aneesrao70@gmail.com>
2023-08-20 11:39:02 +0500

    port change 2

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const backendPort = 5000; // Replace this with your actual backend port
 
 const api = axios.create({
-  baseURL: `https://inventorbackend.onrender.com`,
+  baseURL: `https://mybackend-023u.onrender.com`,
   /* http://localhost:${backendPort} */
 });
 
